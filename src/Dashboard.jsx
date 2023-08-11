13b8f2c1cd6af301a564b99b25260e67ab13de9a
Lisa Knox <lisaknox83@gmail.com>
2023-08-11 16:44:21 -0400

    table kinda works?

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,15 @@
+import ButtonAppBar from './ButtonAppBar';
+import BasicTable from './BasicTable';
+import { mockTransactions } from '../mockData';
+
 const Dashboard = () => {
-  return <section id='dashboard'>Dashboard</section>;
+  return (
+    <section id='dashboard'>
+      <h1>Dashboard</h1>
+      <ButtonAppBar />
+      <BasicTable passedInProps={mockTransactions} />
+    </section>
+  );
 };
 
 export default Dashboard;
